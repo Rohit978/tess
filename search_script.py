55dbdef1309f06ee7ad2a5ccc1d62cc76e62b9fe
Rohit <01rohitkumar0104@gmail.com>
2026-02-16 17:08:54 +0530

    Cleanup: Removed temporary debug scripts

--- search_script.py
+++
@@ -1,7 +0,0 @@
-from ddgs import DDGS
-
-with DDGS() as ddgs:
-    results = [r['body'] for r in ddgs.text('Narendra Modi early life biography')]
-
-with open('modi_bio.txt', 'w', encoding='utf-8') as f:
-    f.write('\n'.join(results))
